8f2dad8e293dd28d89d90dd731a1c0c5ce5d5f94
arseniy1245 <91469792+arseniy1245@users.noreply.github.com>
2022-09-29 16:17:25 +0300

    a5 (iOS)

--- js/a5.js
+++ js/a5.js
@@ -1,1 +1,19 @@
-Test 
+const fs = require('fs');
+
+const dir = './data/klines/csv/1h'
+const files = fs.readdirSync(dir);
+
+var ttlP = 0;
+
+var ii = 0;
+for (let file_name of files) {
+    let ARR = fs.readFileSync(dir + '/' + file_name, 'utf-8').split('\n');
+
+for(let i = 0; i < ARR.length; i++){
+	let lineArr = ARR[i].split(",");
+	
+	ttlP += (lineArr[3] / lineArr[1] - 1);
+	ii++;
+	}
+}
+console.log((ttlP / ii).toFixed(5));
